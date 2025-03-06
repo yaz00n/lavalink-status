ba4b1024ab0a05205c51b56ea092fca9eb8a08fa
Gaurav <81549793+gaurav87565@users.noreply.github.com>
2025-03-06 18:53:45 +0530

    better config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 module.exports = {
   token: process.env.token || "", // Your bot token
   channelId: process.env.channelId || "", //Channel Id where the message will be sent
+
   webMonitor: true, // Set to false if you don't want a  website
   expressPort: process.env.expressPort || 3000, // Port for website
 
@@ -11,6 +12,15 @@ module.exports = {
       port: , // Your lavalink port
       identifier: "", // Name for your lavalink
       secure: false, // set too true if your lavalink has SSL
+      reconnectTimeout: 300000,
+      reconnectTries: 100,
+    },
+    {
+      host: "",  
+      password: "", 
+      port: , 
+      identifier: "",
+      secure: false,
       reconnectTimeout: 300000,
       reconnectTries: 100,
     },
