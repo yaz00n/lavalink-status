a120e83353ba06f07f2a563a54145972d8caa00e
Gaurav <81549793+gaurav87565@users.noreply.github.com>
2025-03-06 18:59:12 +0530

    updated config.js

--- src/config.js
+++ src/config.js
@@ -1,17 +1,17 @@
 module.exports = {
-  token: process.env.token || "", // Your bot token
-  channelId: process.env.channelId || "", //Channel Id where the message will be sent
+  token: process.env.token || "", // Ensure this env variable is set
+  channelId: process.env.channelId || "",
 
-  webMonitor: true, // Set to false if you don't want a  website
-  expressPort: process.env.expressPort || 3000, // Port for website
+  webMonitor: true, // Set to false if you don't want a website
+  expressPort: process.env.expressPort || 3000,
 
   nodes: [
     {
-      host: "", // Your lavalink host address 
-      password: "", // Your lavalink password
-      port: , // Your lavalink port
-      identifier: "", // Name for your lavalink
-      secure: false, // set too true if your lavalink has SSL
+      host: "your-lavalink-host.com", // Your lavalink host address 
+      password: "your-password", // Your lavalink password
+      port: 2333, // Your lavalink port
+      identifier: "Node", // Name for your lavalink
+      secure: false, // set to true if your lavalink has SSL
       reconnectTimeout: 300000,
       reconnectTries: 100,
     },
