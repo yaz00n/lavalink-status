be8d3ecd67be12119e2acf5e3a0a7e503671c01e
Gaurav <81549793+gaurav87565@users.noreply.github.com>
2025-03-06 20:54:06 +0530

    Readme updated

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   token: process.env.token || "", // Ensure this env variable is set
-  channelId: process.env.channelId || "", // Channel Id where the message is to be shown
+  channelId: process.env.channelId || "",
 
   webMonitor: true, // Set to false if you don't want a website
   expressPort: process.env.expressPort || 3000,
