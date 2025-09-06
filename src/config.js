9e48f95fba1cbf5fadc350335682ec3645ac269f
Yazoon <101861409+yaz00n@users.noreply.github.com>
2025-09-06 16:26:40 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   token: process.env.token || "", // Ensure this env variable is set
-  channelId: process.env.channelId || "1408704855698182224",
+  channelId: process.env.channelId || "1412831816309669998",
 
   webMonitor: true, // Set to false if you don't want a website
   expressPort: process.env.expressPort || 3000,
@@ -8,18 +8,18 @@ module.exports = {
   nodes: [
     {
       host: "193.226.78.187", // Your lavalink host address 
-      password: "novaloungehosting", // Your lavalink password
+      password: "newgenstudio", // Your lavalink password
       port: 6473, // Your lavalink port
-      identifier: "Nova Lounge lavalink", // Name for your lavalink
+      identifier: "NewGen Studio Lavalink", // Name for your lavalink
       secure: false, // set to true if your lavalink has SSL
       reconnectTimeout: 300000,
       reconnectTries: 100,
     },
     {
       host: "193.226.78.187",  
-      password: "novaloungehosting", 
+      password: "newgenstudio", 
       port: 6473, 
-      identifier: "Nova Lounge lavalink",
+      identifier: "NewGen Studio Lavalink",
       secure: false,
       reconnectTimeout: 300000,
       reconnectTries: 100,
