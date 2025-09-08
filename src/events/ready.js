855a29fbb99324330417ba44e804612ef6e4a982
Yazoon <101861409+yaz00n@users.noreply.github.com>
2025-09-08 17:55:47 +0300

    Update ready.js

--- src/events/ready.js
+++ src/events/ready.js
@@ -125,15 +125,5 @@ module.exports = async (client) => {
 
   setInterval(updateLavalinkStats, 60000);
 
-  client.user.setPresence({
-    status: "online",
-    activities: [
-      {
-        name: "Order Manger 🏪",
-        type: ActivityType.Playing,
-      },
-    ],
-  });
-
   console.log(colors.green(`[CLIENT] ${client.user.username} is now Online!`));
 };
